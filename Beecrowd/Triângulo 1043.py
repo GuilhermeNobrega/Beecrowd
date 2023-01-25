39cba38092b912ee09919f5cf7045acb3bb25e35
Guilherme Nóbrega <dantasguilherme434@gmail.com.br>
2023-01-25 18:07:40 -0300

    "Initial commit"

--- Beecrowd/Triângulo 1043.py
+++ Beecrowd/Triângulo 1043.py
@@ -1,14 +1,8 @@
 A,B,C = map(float,input().split())
-maior_lado = max(A,B,C)
-print(maior_lado)
-
-
-
-
-A,B,C = map(float,input().split())
-if C>A-B and C<B+A and B>C-A and B<A+C and A>C-B and A<B+C:
-    print("Perimetro:",A+B+C)
+if A<B+C and B<C+A and C<A+B:
+    perimetro = A+B+C
+    print(f"Perimetro = {perimetro:.1f}")
 else:
     formula = (A+B)*C/2
-    print(f"Area = {formula}")
+    print(f"Area = {formula:.1f}")
 
